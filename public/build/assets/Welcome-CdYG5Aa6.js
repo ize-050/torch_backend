4ea0ac3ca71bf8f3bfcf087f3a664339bd9e3dcf
device_device <dev.ice.3sstech@gmail.com>
2024-04-24 20:26:16 +0700

    update

---
+++ public/build/assets/Welcome-CdYG5Aa6.js
@@ -0,0 +1,1 @@
+import{j as e,a,T as s}from"./app-tKDJcObQ.js";function n({auth:t,laravelVersion:r,phpVersion:o}){return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Welcome"}),e.jsx(s,{})]})}export{n as default};
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\BannerController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CatgoryController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,11 +13,29 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    //     return $request->user();
+    // });
+
+    Route::group(['middleware' => 'cors'], function () {
+    Route::get('getallMenuProducts', [CatgoryController::class, 'getallMenuProducts']);
+
+    Route::get('getAllproductByid/{id}', [ProductController::class, 'getAllproductByid']);
+
+    Route::get('getAllproduct', [ProductController::class, 'getAllproduct']);
+
+    Route::get('getProductDetail/{id}', [ProductController::class, 'getProductDetail']);
+
+    Route::post('product/images/{id}', [ProductController::class, 'updateImages']);
+
+    Route::delete('product/images/{id}', [ProductController::class, 'deleteImages']);
+
+    Route::post('banner/images/{id}', [BannerController::class, 'updateImages']);
+
+    Route::delete('banner/images/{id}', [BannerController::class, 'deleteImages']);
+    });
