356bef9187e16025ffca9316a871c23e4a0f2d5a
device_device <dev.ice.3sstech@gmail.com>
2024-05-26 14:44:32 +0700

    up
    upload

--- resources/js/Pages/Product/Add.jsx
+++ resources/js/Pages/Product/Add.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, React } from "react"
+import { useEffect, useState, React,useCallback } from "react"
 import { useForm, Controller } from "react-hook-form"
 import { router } from '@inertiajs/react'
 import Swal from 'sweetalert2'
@@ -43,20 +43,26 @@ const img = {
 export default function Add({ catgory }) {
     const { handleSubmit, setValue, control, formState: { errors }, } = useForm()
     const [files, setFiles] = useState([]);
-    const { getRootProps, getInputProps } = useDropzone({
-        accept: {
-            'image/*': []
-        },
-        onDrop: acceptedFiles => {
-            setFiles(acceptedFiles.map(file => Object.assign(file, {
-                preview: URL.createObjectURL(file)
-            })));
-        }
-    });
 
-    const handleDelete = (name) => {
-        setFiles(files.filter(file => file.name !== name))
-    }
+    const onDrop = useCallback(acceptedFiles => {
+      const newFiles = acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      }));
+
+    console.log('file',files)
+      setFiles(prevFiles => [...prevFiles, ...newFiles]);
+    }, []);
+  
+    const handleDelete = useCallback((fileName) => {
+      setFiles(prevFiles => prevFiles.filter(file => file.name !== fileName));
+    }, []);
+  
+    const { getRootProps, getInputProps } = useDropzone({
+      accept: {
+        'image/*': []
+      },
+      onDrop
+    });
 
     const thumbs = files.map(file => (
         <div style={thumb} key={file.name}>
@@ -122,7 +128,7 @@ export default function Add({ catgory }) {
                             <div className="col-sm-6">
                                 <ol className="breadcrumb float-sm-right">
                                     <li className="breadcrumb-item"><a href="#">หน้าแรก</a></li>
-                                    <li className="breadcrumb-item active">สินค้า</li>
+                                    <li className="breadcrumb-item active">สินค้า1</li>
 
                                 </ol>
                             </div>
@@ -245,36 +251,40 @@ export default function Add({ catgory }) {
                                 <Controller
                                     control={control}
                                     name="product_images"
-                                    render={({ field: { onChange, onBlur, value, ref } }) => (
+                                    render={({ field: { onChange, value, ref } }) => (
                                         <section className="container">
                                             <div {...getRootProps({ className: 'dropzone' })}>
-                                                <input {...getInputProps()} />
-                                                <div class="w-[400px] relative border-2 border-gray-300 border-dashed rounded-lg p-6" id="dropzone">
-                                                    <input type="file" class="absolute inset-0 w-full h-full opacity-0 z-50" />
-                                                    <div class="text-center">
-                                                        <img class="mx-auto h-12 w-12" src="https://www.svgrepo.com/show/357902/image-upload.svg" alt="" />
+                                                <input {...getInputProps()} onChange={e => {
+                                                    const files = Array.from(e.target.files);
+                                                    onDrop(files);
+                                                    onChange(files);
+                                                }} />
+                                                <div className="w-[400px] relative border-2 border-gray-300 border-dashed rounded-lg p-6" id="dropzone">
+                                                    <input type="file" className="absolute inset-0 w-full h-full opacity-0 z-50" />
+                                                    <div className="text-center">
+                                                        <img className="mx-auto h-12 w-12" src="https://www.svgrepo.com/show/357902/image-upload.svg" alt="" />
 
-                                                        <h3 class="mt-2 text-sm font-medium text-gray-900">
-                                                            <label for="file-upload" class="relative cursor-pointer">
+                                                        <h3 className="mt-2 text-sm font-medium text-gray-900">
+                                                            <label htmlFor="file-upload" className="relative cursor-pointer">
                                                                 <span>Drag and drop</span>
-                                                                <span class="text-indigo-600"> or browse</span>
+                                                                <span className="text-indigo-600"> or browse</span>
                                                                 <span>to upload</span>
-                                                                <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                                                                <input id="file-upload" name="file-upload" type="file" className="sr-only" />
                                                             </label>
                                                         </h3>
-                                                        <p class="mt-1 text-xs text-gray-500">
-                                                            PNG, JPG, GIF up to 10MB
+                                                        <p className="mt-1 text-xs text-gray-500">
+                                                            PNG, JPG, GIF up to 5MB
                                                         </p>
                                                     </div>
-
-                                                    <img src="" class="mt-4 mx-auto max-h-40 hidden" id="preview" />
+                                                    <img src="" className="mt-4 mx-auto max-h-40 hidden" id="preview" />
                                                 </div>
                                             </div>
                                             <aside style={thumbsContainer}>
                                                 {thumbs}
                                             </aside>
                                         </section>
-                                    )} />
+                                    )}
+                                />
                             </div>
                         </div>
                         <div className="my-16 p-3">
